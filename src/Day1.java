f818aa914e156b4e4debc18b1d63fc9ad914f3ef
Om <m425.sharma@gmail.com>
2026-07-09 14:35:35 +0530

    added samples

---
+++ src/Day1.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Day1 {
+    public static void main(String[] args) {
+        String name = "Prerit Singh";
+        int age = 20;
+        String college = "R.D. Engineering College";
+        String course = "Java Advance";
+        String hobby = "Gym";
+
+        System.out.println("Hi Mam, My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("I passed my graduation from " + college);
+        System.out.println("My course name is " + course);
+        System.out.println("My hobby is " + hobby);
+    }
+}
